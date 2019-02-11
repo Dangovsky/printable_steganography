6d0e44bbbbf449f7e3c7b90643dcd1fff82fb2ae
Dangovsky <tenkovdaniil@gmail.com>
2019-02-11 22:20:56 +0300

    add hide and find showcase

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@
 using namespace std;
 using namespace cv;
 
-int RGBtoCMYCtoRGB_test()
+void RGBtoCMYCtoRGB_test()
 {
     Fujitsu_algo f;
 
-    Mat pure_image = imread("lena.png", cv::IMREAD_COLOR);
+    Mat pure_image = imread("lena.png", IMREAD_COLOR);
     namedWindow( "Pure image", WINDOW_AUTOSIZE );
     imshow( "Pure image", pure_image );
 
@@ -33,12 +33,49 @@ int RGBtoCMYCtoRGB_test()
     for (it = diff_image.begin<Vec3f>(), end = diff_image.end<Vec3f>(); it != end; it++){
         sum += (*it)[2] + (*it)[1] + (*it)[0];
     }
-    cout << sum;
+    cout << "Simple difference between images before and after transform is " << sum;
+
+    waitKey(0);
+}
+
+void Stego_test(){
+    Fujitsu_algo f;
+
+    Mat pure_image = imread("lena.png", IMREAD_COLOR);
+    namedWindow( "Pure image", WINDOW_AUTOSIZE );
+    imshow( "Pure image", pure_image );
+
+    Mat qr = imread("qr.png", IMREAD_GRAYSCALE);
+    namedWindow( "qr", WINDOW_AUTOSIZE );
+    imshow( "qr", qr );
+
+    waitKey(0);
+
+    Mat container = f.HideQR(pure_image, qr);
+    namedWindow( "container", WINDOW_AUTOSIZE );
+    imshow( "container", container );
+
+    Mat diff_image;
+    absdiff(container, pure_image, diff_image);
+    namedWindow( "Diff image", WINDOW_AUTOSIZE );
+    imshow( "Diff image", diff_image );
+    MatIterator_<Vec3f> it, end;
+    long sum = 0;
+    for (it = diff_image.begin<Vec3f>(), end = diff_image.end<Vec3f>(); it != end; it++){
+        sum += (*it)[2] + (*it)[1] + (*it)[0];
+    }
+    cout << "Simple difference between images before and after hiding is " << sum;
+
+    waitKey(0);
+
+    Mat qr_from_container = f.FindQR(container, f.getBlockW(), f.getBlockH());
+    namedWindow( "qr from container", WINDOW_AUTOSIZE );
+    imshow( "qr from container", qr_from_container );
 
     waitKey(0);
 }
 
 int main()
 {
-    RGBtoCMYCtoRGB_test();
+    Stego_test();
 }
